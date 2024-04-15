e9cd365d24a5617a8fa0ec1b0e7491d21ba0aace
IVBadalov21 <IVBadalov21@codingburgas.bg>
2024-04-15 16:41:36 +0300

    Write example program

--- Bis/src/main.cpp
+++ Bis/src/main.cpp
@@ -1,13 +1,23 @@
+#include <string>
 #include "raylib.h"
+
+using namespace std;
 
 int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "School Test Program");
 
     SetTargetFPS(60);
+
+    string question = "What is the capital of France?";
+    string correctAnswer = "Paris";
+    string answerInput;
+
+    int resultTimer = 0;
+    int resultDuration = 120;
 
     while (!WindowShouldClose())
     {
@@ -15,8 +25,37 @@ int main()
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText(question.c_str(), 100, 100, 20, DARKGRAY);
+        DrawText("Your answer: ", 100, 150, 20, DARKGRAY);
+        DrawText(answerInput.c_str(), 250, 150, 20, DARKGRAY);
+
+        int key = GetCharPressed();
+
+        if (key > 0 and answerInput.length() < 99)
+            answerInput.push_back((char)key);
+
+        if (IsKeyPressed(KEY_BACKSPACE) and !answerInput.empty())
+            answerInput.pop_back();
+        
+
+        if (IsKeyPressed(KEY_ENTER))
+            resultTimer = resultDuration;
+ 
+
+        if (resultTimer > 0)
+        {
+            if (answerInput == correctAnswer)
+                DrawText("Correct!", 100, 200, 20, GREEN);
+            
+            else
+                DrawText("Incorrect. Please try again.", 100, 200, 20, RED);
+       
+            resultTimer--;
+        }
 
         EndDrawing();
     }
+
+    CloseWindow();
+
 }
