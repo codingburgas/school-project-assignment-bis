1955770996aeab20811260187c37a716f7db9cb9
IVBadalov21 <IVBadalov21@codingburgas.bg>
2024-04-21 15:47:14 +0300

    Add geography test

--- Bis/src/main.cpp
+++ Bis/src/main.cpp
@@ -7,8 +7,32 @@ bool inGame = false;
 
 void Geography()
 {
-    exitWindow = true;
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    static const char* question = "What is the capital of France?";
+    static const char* correctAnswer = "Paris";
+    static std::string answer = "";
+    DrawText(question, 100, 100, 30, DARKGRAY);
+    DrawText("Your Answer: ", 100, 150, 20, DARKGRAY);
+    DrawRectangleLines(300, 150, 200, 30, DARKGRAY);
+    DrawText(answer.c_str(), 305, 155, 20, DARKGRAY);
+    if (!IsKeyDown(KEY_ENTER))
+    {
+        char keyPressed = GetKeyPressed();
+        if (keyPressed != 0)
+        {
+            answer += keyPressed;
+        }
+    }
+    if (IsKeyPressed(KEY_ENTER))
+    {
+        if (answer == correctAnswer)
+            DrawText("Correct!", 100, 200, 20, GREEN);
+        else
+            DrawText("Incorrect! The correct answer is Paris.", 100, 200, 20, RED);
+    }
 }
+
 
 void Maths()
 {
@@ -25,6 +49,22 @@ void Settings()
 {
     exitWindow = true;
 }
+void StartMenu() {
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("School Test Program", 100, 100, 40, GRAY);
+    DrawText(selectedOption == 0 ? "> Start" : "Start", 100, 200, 20, DARKGRAY);
+    DrawText(selectedOption == 1 ? "> Settings" : "Settings", 100, 240, 20, DARKGRAY);
+    DrawText(selectedOption == 2 ? "> Exit" : "Exit", 100, 280, 20, DARKGRAY);
+
+    if (IsKeyPressed(KEY_UP) && selectedOption >= 0)
+        selectedOption--;
+
+    if (IsKeyPressed(KEY_DOWN) && selectedOption <= 2)
+        selectedOption++;
+}
+
 void SecondMenu() {
 
     ClearBackground(RAYWHITE);
@@ -39,13 +79,16 @@ void SecondMenu() {
 
     if (IsKeyPressed(KEY_ENTER))
     {
-        if (selectedOption == 0) {
+        if (selectedOption == 0)
+        {
             Geography();
         }
-        else if (selectedOption == 1) {
+        else if (selectedOption == 1)
+        {
             Maths();
         }
-        else if (selectedOption == 2) {
+        else if (selectedOption == 2)
+        {
             History();
         }
     }
@@ -57,21 +100,6 @@ void SecondMenu() {
     if (IsKeyPressed(KEY_DOWN) && selectedOption < 2) {
         selectedOption++;
     }
-}
-void StartMenu() {
-
-    ClearBackground(RAYWHITE);
-
-    DrawText("School Test Program", 100, 100, 40, GRAY);
-    DrawText(selectedOption == 0 ? "> Start" : "Start", 100, 200, 20, DARKGRAY);
-    DrawText(selectedOption == 1 ? "> Settings" : "Settings", 100, 240, 20, DARKGRAY);
-    DrawText(selectedOption == 2 ? "> Exit" : "Exit", 100, 280, 20, DARKGRAY);
-
-    if (IsKeyPressed(KEY_UP) and selectedOption >= 0)
-        selectedOption--;
-
-    if (IsKeyPressed(KEY_DOWN) and selectedOption <= 2)
-        selectedOption++;
 }
 
 int main()
