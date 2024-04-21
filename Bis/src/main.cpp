069c152b03d57e3aa7b502232b1a23bc94f84e50
IVBadalov21 <IVBadalov21@codingburgas.bg>
2024-04-21 15:37:16 +0300

    Add three topics for test

--- Bis/src/main.cpp
+++ Bis/src/main.cpp
@@ -1,16 +1,63 @@
 ﻿#include <iostream>
 #include "raylib.h"
 
-void StartMenu();
-void SecondMenu();
-void Game();
-void Settings();
-
 bool exitWindow = false;
 int selectedOption = 0;
 bool inGame = false;
 
+void Geography()
+{
+    exitWindow = true;
+}
 
+void Maths()
+{
+    exitWindow = true;
+}
+
+void History()
+{
+    exitWindow = true;
+}
+
+
+void Settings()
+{
+    exitWindow = true;
+}
+void SecondMenu() {
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("School Subjects", 100, 100, 40, GRAY);
+
+    const char* subjects[] = { "Geography", "Maths", "History" };
+
+    for (int i = 0; i < 3; i++) {
+        DrawText(selectedOption == i ? (std::string("> ") + subjects[i]).c_str() : subjects[i], 100, 200 + i * 40, 20, DARKGRAY);
+    }
+
+    if (IsKeyPressed(KEY_ENTER))
+    {
+        if (selectedOption == 0) {
+            Geography();
+        }
+        else if (selectedOption == 1) {
+            Maths();
+        }
+        else if (selectedOption == 2) {
+            History();
+        }
+    }
+
+    if (IsKeyPressed(KEY_UP) && selectedOption > 0) {
+        selectedOption--;
+    }
+
+    if (IsKeyPressed(KEY_DOWN) && selectedOption < 2) {
+        selectedOption++;
+    }
+}
 void StartMenu() {
 
     ClearBackground(RAYWHITE);
@@ -20,77 +67,16 @@ void StartMenu() {
     DrawText(selectedOption == 1 ? "> Settings" : "Settings", 100, 240, 20, DARKGRAY);
     DrawText(selectedOption == 2 ? "> Exit" : "Exit", 100, 280, 20, DARKGRAY);
 
-    if (IsKeyPressed(KEY_UP) && selectedOption >= 0)
+    if (IsKeyPressed(KEY_UP) and selectedOption >= 0)
         selectedOption--;
 
-    if (IsKeyPressed(KEY_DOWN) && selectedOption <= 2)
+    if (IsKeyPressed(KEY_DOWN) and selectedOption <= 2)
         selectedOption++;
-}
-void SecondMenu() {
-    ClearBackground(RAYWHITE);
-    selectedOption = 0;
-    DrawText("School Subjects", 100, 100, 40, GRAY);
-    
-    DrawText(selectedOption == 0 ? "> Geography" : "Geography", 100, 200, 20, DARKGRAY);
-    DrawText(selectedOption == 1 ? "> Maths" : "Maths", 100, 240, 20, DARKGRAY);
-    DrawText(selectedOption == 2 ? "> History" : "History", 100, 280, 20, DARKGRAY);
-
-    if (IsKeyPressed(KEY_UP) && selectedOption >= 0)
-        selectedOption--;
-
-    if (IsKeyPressed(KEY_DOWN) && selectedOption <= 2)
-        selectedOption++;
-
-    
-}
-void Game()
-{
-    BeginDrawing();
-
-    ClearBackground(RAYWHITE);
-
-    static const char* question = "What is the capital of France?";
-    static const char* correctAnswer = "Paris";
-    static std::string answer = "";
-
-    DrawText(question, 100, 100, 30, DARKGRAY);
-    DrawText("Your Answer: ", 100, 150, 20, DARKGRAY);
-
-    DrawRectangleLines(300, 150, 200, 30, DARKGRAY);
-    DrawText(answer.c_str(), 305, 155, 20, DARKGRAY);
-
-
-    if (!IsKeyDown(KEY_ENTER))
-    {
-        char keyPressed = GetKeyPressed();
-
-        if (keyPressed != 0)
-        {
-            answer += keyPressed;
-        }
-    }
-
-    if (IsKeyPressed(KEY_ENTER))
-    {
-        if (answer == correctAnswer)
-            DrawText("Correct!", 100, 200, 20, GREEN);
-        else
-            DrawText("Incorrect! The correct answer is Paris.", 100, 200, 20, RED);
-    }
-
-//    EndDrawing();
-}
-
-void Settings()
-{
-    exitWindow = true;
 }
 
 int main()
 {
-    InitWindow(1920, 1080, "School test program");
-    SetWindowState(FLAG_FULLSCREEN_MODE);
-
+    InitWindow(800, 450, "School test program");
 
     while (!exitWindow)
     {
@@ -104,23 +90,24 @@ int main()
         }
         if (IsKeyPressed(KEY_ENTER))
         {
-            switch (selectedOption)
+            if (selectedOption == 0)
             {
-            case 0:
                 inGame = true;
-                break;
-            case 1:
-                Settings();
-                break;
-            case 2:
+            }
+            else if (selectedOption == 1)
+            {
+               Settings();
+            }
+            else if (selectedOption == 2)
+            {
                 exitWindow = true;
-                break;
             }
         }
-        
+
+        if (IsKeyDown(KEY_ESCAPE)) {
+            exitWindow = true;
+        }
+
         EndDrawing();
     }
-    CloseWindow();
-
-    return 0;
 }
